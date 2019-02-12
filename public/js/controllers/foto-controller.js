ead26a1d5e569ff936d6dc92eeaa363c64a60593
Camila QAT PC <camila.rodrigues@outlook.com.br>
2019-02-12 14:51:30 -0500

    Adicionando as funcionalidades de Remoção e Edição das fotos.

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,11 +1,37 @@
-angular.module("alurapic").controller("FotoController", function($scope, $http) {
+angular.module("alurapic").controller("FotoController", function($scope, $http, $routeParams) {
     $scope.foto = {};
     $scope.mensagem = "";
 
+    if ($routeParams.fotoId) {
+        $http.get("/v1/fotos/" + $routeParams.fotoId)
+        .success(function(foto) {
+            $scope.foto = foto;
+        })
+        .error(function(erro) {
+            console.log(erro);
+            $scope.mensagem = "Não foi possível localizar a foto"
+        });
+    }
+
     $scope.submeter = function() {
         
-        if ($scope.formulario.$valid)
-        {
+        if ($scope.formulario.$valid) {
+            
+            if ($routeParams.fotoId) {
+
+                $http.put("/v1/fotos/" + $routeParams.fotoId, $scope.foto)
+                .success(function() {
+                    $scope.mensagem = "Foto " + $scope.foto.titulo + " atualizada com sucesso";
+                    $scope.foto = {};
+                })
+                .error(function() {
+                    $scope.mensagem = "Não foi possível atualizar a foto";
+                    console.log(erro);
+                });
+
+                return;
+            }
+
             $http.post("/v1/fotos", $scope.foto)
             .success(function() {
                 $scope.foto = {};
